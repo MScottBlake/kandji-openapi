2c69c55d8b9e251cff7eb98a45894fb3c08f1a37
Scott Blake <945258+MScottBlake@users.noreply.github.com>
2025-02-17 11:53:05 -0500

    Create method to generate spec

--- src/kandji_openapi/openapi_generator.py
+++ src/kandji_openapi/openapi_generator.py
@@ -2,13 +2,19 @@ import json
 from pathlib import Path
 
 from models.postman_collection import PostmanCollection
+from openapi_pydantic import OpenAPI
 from ruamel.yaml import YAML
 
 
 class OpenAPIGenerator:
     def __init__(self, collection: PostmanCollection) -> None:
         self.collection = collection
-        self.openapi_spec = collection.to_openapi()
+        self.openapi_spec = self._generate_openapi_spec()
+
+    def _generate_openapi_spec(self) -> OpenAPI:
+        """Generate OpenAPI spec with components"""
+        spec = self.collection.to_openapi()
+        return spec
 
     def to_json(self, file_path: Path) -> None:
         """Write OpenAPI spec to JSON file"""
